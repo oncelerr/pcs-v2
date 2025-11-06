417c46a0b7032ed96e5156f7f0de07ba72e0f441
oncelerr <markjonathan368@gmail.com>
2025-11-06 15:28:53 +0800

    update for prod

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -60,8 +60,8 @@ class NotificationController extends Controller
         
         // Send email notification to the email address specified in MAIL_NOTIF env variable
         try {
-            // $notificationEmail = "filings@premiumcorpsolutions.com";
-            $notificationEmail = "markjonathan368@gmail.com";
+            $notificationEmail = "filings@premiumcorpsolutions.com";
+            // $notificationEmail = "markjonathan368@gmail.com";
             
             if ($notificationEmail) {
                 Mail::send([], [], function ($mailMessage) use ($notificationEmail, $title, $message, $options) {
